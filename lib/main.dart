5c242834f131a8411220cd8a180120fd78d69885
Masahiro Aoki <rafe.kun@gmail.com>
2021-06-09 01:44:56 +0900

    update bundle id and app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:fluid/app/welcome/welcome_page.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -12,7 +16,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demos',
-      theme: ThemeData.dark(),
       home: WelcomePage(),
     );
   }
